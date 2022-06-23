210eab0b5541994a65cd693dce1fa1c76928fbb5
Kojiro Sugiyama <rogozarski.ik3@gmail.com>
2022-06-23 22:39:32 +0900

    first commit

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\User;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testSuccessfulLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $user = User::factory()->create(); // テスト用のユーザーを作成する
+            $browser->visit('/login') // パスを変更する
+                ->type('email', $user->email) // メールアドレスを入力する
+                ->type('password', 'password') // パスワードを入力する
+                ->press('LOG IN') // 「LOG IN」ボタンをクリックする
+                ->assertPathIs('/tweet') // /tweetに遷移したことを確認する
+                ->assertSee('つぶやきアプリ');
+        });
+    }
+}
